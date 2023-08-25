9b8811deea95a971fbde74ce9734a33793ffe55d
locthn <loc.thnguyen@katalon.com>
2023-08-25 15:39:18 +0700

    init project

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,47 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+
+
+def static "com.kms.katalon.keyword.draganddrop.DragAndDropKeywords.dragAndDropHtml5"(
+    	TestObject sourceObject	
+     , 	TestObject destinationObject	) {
+    (new com.kms.katalon.keyword.draganddrop.DragAndDropKeywords()).dragAndDropHtml5(
+        	sourceObject
+         , 	destinationObject)
+}
+
+
+def static "com.kms.katalon.keyword.draganddrop.DragAndDropKeywords.dragAndDropJquery"(
+    	TestObject sourceObject	
+     , 	TestObject destinationObject	) {
+    (new com.kms.katalon.keyword.draganddrop.DragAndDropKeywords()).dragAndDropJquery(
+        	sourceObject
+         , 	destinationObject)
+}
+
+
+def static "com.kms.katalon.keyword.draganddrop.DragAndDropKeywords.dragAndDropBy"(
+    	TestObject sourceObject	
+     , 	TestObject destinationObject	
+     , 	int xOffset	
+     , 	int yOffset	) {
+    (new com.kms.katalon.keyword.draganddrop.DragAndDropKeywords()).dragAndDropBy(
+        	sourceObject
+         , 	destinationObject
+         , 	xOffset
+         , 	yOffset)
+}
+
+
+def static "com.kms.katalon.keyword.draganddrop.DragAndDropKeywords.dragAndDrop"(
+    	TestObject sourceObject	
+     , 	TestObject destinationObject	) {
+    (new com.kms.katalon.keyword.draganddrop.DragAndDropKeywords()).dragAndDrop(
+        	sourceObject
+         , 	destinationObject)
+}
